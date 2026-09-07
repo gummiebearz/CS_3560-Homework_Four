fa2b80b9609cd0ac37bc796ef094d4b9a0316980
Vivian Pham <huynhp@cpp.edu>
2026-09-06 21:22:03 -0700

    Initial VendorInvoice class

---
+++ interface/VendorInvoice.java
@@ -0,0 +1,48 @@
+public class VendorInvoice implements Payable {
+    private String vendorName;
+    private String invoiceNumber;
+    private double amountDue;
+
+    public VendorInvoice(String newVendorName, String newInvoiceNumber, double newAmountDue) {
+        vendorName = newVendorName;
+        invoiceNumber = newInvoiceNumber;
+        setAmountDue(newAmountDue);
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public double getAmountDue() {
+        return amountDue;
+    }
+
+    public void setVendorName(String newVendorName) {
+        vendorName = newVendorName;
+    }
+
+    public void setInvoiceNumber(String newInvoiceNumber) {
+        invoiceNumber = newInvoiceNumber;
+    }
+
+    public void setAmountDue(double newAmountDue) {
+        if(newAmountDue < 0) {
+            throw IllegalArgumentException("Amount due must be non-negative");
+        }
+        amountDue = newAmountDue;
+    }
+
+    @Override
+    public double calculatePayment() {
+        return amountDue;
+    }
+
+    @Override
+    public String getPayeeName() {
+        return vendorName;
+    }
+}
