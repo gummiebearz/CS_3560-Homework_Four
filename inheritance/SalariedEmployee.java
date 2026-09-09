44c02be51787b2bd9dea7fbff7276bd5c0e52799
Vivian Pham <huynhp@cpp.edu>
2026-09-09 14:54:06 -0700

    Fixed spacings

--- inheritance/SalariedEmployee.java
+++ inheritance/SalariedEmployee.java
@@ -18,6 +18,7 @@ public class SalariedEmployee extends Employee {
   public void display() {
     System.out.println("Salaried Employee:");
     System.out.println("------------------");
+
     super.display();
     System.out.printf("Weekly salary: $%.0f\n", this.weeklySalary);
   }
