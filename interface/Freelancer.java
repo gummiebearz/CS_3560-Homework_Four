372277bf41807bd55f4dba9ddf0ce00ad404fe4f
Vivian Pham <huynhp@cpp.edu>
2026-09-08 23:49:06 -0700

    Add "this" reference to member variables

--- interface/Freelancer.java
+++ interface/Freelancer.java
@@ -1,79 +1,79 @@
 public class Freelancer implements Payable {
-    private String firstName;
-    private String lastName;
-    private double hourlyRate;
-    private double hoursWorked;
+  private String firstName;
+  private String lastName;
+  private double hourlyRate;
+  private double hoursWorked;
 
-    public Freelancer(String newFirstName, String newLastName, double newHourlyRate, double newHoursWorked) {
-        firstName = newFirstName;
-        lastName = newLastName;
-        setHourlyRate(newHourlyRate);
-        setHoursWorked(newHoursWorked);
+  public Freelancer(String firstName, String lastName, double hourlyRate, double hoursWorked) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.setHourlyRate(hourlyRate);
+    this.setHoursWorked(hoursWorked);
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public double getHourlyRate() {
+    return this.hourlyRate;
+  }
+
+  public double getHoursWorked() {
+    return this.hoursWorked;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setHourlyRate(double newHourlyRate) {
+    if (newHourlyRate < 0) {
+      throw new IllegalArgumentException("Rate must be non-negative");
     }
 
-    public String getFirstName() {
-        return firstName;
+    this.hourlyRate = newHourlyRate;
+  }
+
+  public void setHoursWorked(double newHoursWorked) {
+    if (newHoursWorked < 0) {
+      throw new IllegalArgumentException("Hours worked must be non-negative");
     }
 
-    public String getLastName() {
-        return lastName;
+    this.hoursWorked = newHoursWorked;
+  }
+
+  @Override
+  public double calculatePayment() {
+    double payment = 0;
+
+    if (this.hoursWorked <= 40) {
+      payment = this.hourlyRate * this.hoursWorked;
+    } else {
+      double regularPayment = 40 * this.hourlyRate;
+      double overtimePayment = (this.hoursWorked - 40) * (this.hourlyRate * 1.5);
+      payment = regularPayment + overtimePayment;
     }
 
-    public double getHourlyRate() {
-        return hourlyRate;
-    }
+    return payment;
+  }
 
-    public double getHoursWorked() {
-        return hoursWorked;
-    }
+  @Override
+  public String getPayeeName() {
+    return this.firstName + " " + this.lastName;
+  }
 
-    public void setFirstName(String newFirstName) {
-        firstName = newFirstName;
-    }
-
-    public void setLastName(String newLastName) {
-        lastName = newLastName;
-    }
-
-    public void setHourlyRate(double newHourlyRate) {
-        if(newHourlyRate < 0) {
-            throw new IllegalArgumentException("Rate must be non-negative");
-        } 
-
-        hourlyRate = newHourlyRate;
-    }
-
-    public void setHoursWorked(double newHoursWorked) {
-        if(newHoursWorked < 0) {
-            throw new IllegalArgumentException("Hours worked must be non-negative");
-        } 
-
-        hoursWorked = newHoursWorked;
-    }
-
-    @Override
-    public double calculatePayment() {
-        double payment = 0;
-
-        if(hoursWorked <= 40) {
-            payment = hourlyRate * hoursWorked;
-        } else {
-            double regularPayment = 40 * hourlyRate;
-            double overtimePayment = (hoursWorked - 40) * (hourlyRate * 1.5); 
-            payment = regularPayment + overtimePayment;
-        }
-
-        return payment;
-    }
-
-    @Override
-    public String getPayeeName() {
-        return firstName + " " + lastName;
-    }
-
-    @Override
-    public void print() {
-        System.out.println(getPayeeName());
-        System.out.printf("    -> Payment: $%.2f\n", calculatePayment());
-    }
+  @Override
+  public void print() {
+    System.out.println(this.getPayeeName());
+    System.out.printf("    -> Payment: $%.2f\n", this.calculatePayment());
+  }
 }
--- interface/VendorInvoice.java
+++ interface/VendorInvoice.java
@@ -1,54 +1,54 @@
 public class VendorInvoice implements Payable {
-    private String vendorName;
-    private String invoiceNumber;
-    private double amountDue;
+  private String vendorName;
+  private String invoiceNumber;
+  private double amountDue;
 
-    public VendorInvoice(String newVendorName, String newInvoiceNumber, double newAmountDue) {
-        vendorName = newVendorName;
-        invoiceNumber = newInvoiceNumber;
-        setAmountDue(newAmountDue);
-    }
+  public VendorInvoice(String vendorName, String invoiceNumber, double amountDue) {
+    this.vendorName = vendorName;
+    this.invoiceNumber = invoiceNumber;
+    this.setAmountDue(amountDue);
+  }
 
-    public String getVendorName() {
-        return vendorName;
-    }
+  public String getVendorName() {
+    return this.vendorName;
+  }
 
-    public String getInvoiceNumber() {
-        return invoiceNumber;
-    }
+  public String getInvoiceNumber() {
+    return this.invoiceNumber;
+  }
 
-    public double getAmountDue() {
-        return amountDue;
-    }
+  public double getAmountDue() {
+    return this.amountDue;
+  }
 
-    public void setVendorName(String newVendorName) {
-        vendorName = newVendorName;
-    }
+  public void setVendorName(String vendorName) {
+    this.vendorName = vendorName;
+  }
 
-    public void setInvoiceNumber(String newInvoiceNumber) {
-        invoiceNumber = newInvoiceNumber;
-    }
+  public void setInvoiceNumber(String invoiceNumber) {
+    this.invoiceNumber = invoiceNumber;
+  }
 
-    public void setAmountDue(double newAmountDue) {
-        if(newAmountDue < 0) {
-            throw new IllegalArgumentException("Amount due must be non-negative");
-        }
-        amountDue = newAmountDue;
+  public void setAmountDue(double newAmountDue) {
+    if (newAmountDue < 0) {
+      throw new IllegalArgumentException("Amount due must be non-negative");
     }
+    this.amountDue = newAmountDue;
+  }
 
-    @Override
-    public double calculatePayment() {
-        return amountDue;
-    }
+  @Override
+  public double calculatePayment() {
+    return this.amountDue;
+  }
 
-    @Override
-    public String getPayeeName() {
-        return vendorName;
-    }
+  @Override
+  public String getPayeeName() {
+    return this.vendorName;
+  }
 
-    @Override
-    public void print() {
-        System.out.println(getPayeeName());
-        System.out.printf("    -> Payment: $%.2f\n", calculatePayment());
-    }
+  @Override
+  public void print() {
+    System.out.println(this.getPayeeName());
+    System.out.printf("    -> Payment: $%.2f\n", this.calculatePayment());
+  }
 }
