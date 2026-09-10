53a94dabb30f3b65988ee0c255c823998c49b1ad
Vivian Pham <huynhp@cpp.edu>
2026-09-09 23:34:57 -0700

    Fixed spacings

--- aggregation/Textbook.java
+++ aggregation/Textbook.java
@@ -1,35 +1,35 @@
 public class Textbook {
-    private String title;
-    private String author;
-    private String publisher;
+  private String title;
+  private String author;
+  private String publisher;
 
-    public Textbook(String title, String author, String publisher) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-    }
+  public Textbook(String title, String author, String publisher) {
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+  }
 
-    public String getTitle() {
-        return this.title;
-    }
+  public String getTitle() {
+    return this.title;
+  }
 
-    public String getAuthor() {
-        return this.author;
-    }
+  public String getAuthor() {
+    return this.author;
+  }
 
-    public String getPublisher() {
-        return this.publisher;
-    }
+  public String getPublisher() {
+    return this.publisher;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
+  public void setPublisher(String publisher) {
+    this.publisher = publisher;
+  }
 }
